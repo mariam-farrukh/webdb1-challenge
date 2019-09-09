592f5ac41afc56afb64c87dcb78e0ea15811cbea
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-09 15:43:57 -0500

    Setting up router

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const AccountsRouter = require('./accounts/acounts-router.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', AccountsRouter);
+
+server.get("/", (req, res) => {
+    res.send("<h2>Conntected to Api</h2>");
+});
 
 module.exports = server;
