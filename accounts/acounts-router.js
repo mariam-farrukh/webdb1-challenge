a9283f25d9f1c205e6495f3629214a2b7118caac
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-09 16:24:53 -0500

    MVP Complete: CRUD operations completed

--- accounts/acounts-router.js
+++ accounts/acounts-router.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
       .catch(() => {
         res.status(500).json({ message: 'Could not retrieve the list of accounts' });
       });
-  });
+});
 
 router.get("/:id", (req, res) => {
     const { id } = req.params;
@@ -31,6 +31,58 @@ router.get("/:id", (req, res) => {
           .status(500)
           .json({ message: "Unable to fetch account from database" });
       });
-  });
+});
+
+router.post("/", (req, res) => {
+    const newAccount = req.body;
+    if (!newAccount.name) {
+      res.status(404).json({ message: "Please add an account name" });
+    }
+    if (!newAccount.budget) {
+      res.status(404).json({ message: "Please add an account budget" });
+    }
+    db("accounts")
+    .insert(newAccount, "id")
+    .then(account => {
+        res.status(201).json(account);
+    })
+    .catch(err => {
+        res.status(500).json({ message: "Unable to add account to database" });
+    });
+});
+  
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    db('accounts')
+    .where({ id: id })
+    .update(req.body)
+    .then(count => {
+      if (count) {
+        res.status(200).json({ message: `${count} record(s) updated` });
+      } else {
+        res.status(404).json({ message: 'Account not found' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Could not update the account' });
+    });
+});
+  
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    db("accounts")
+    .where({ id: id })
+    .del()
+    .then(count => {
+        if (count) {
+          res.status(200).json({ message: `${count} account has been deleted` });
+        } else {
+          res.status(404).json({ message: "Account not found" });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "Error removing from database" });
+    });
+});
 
 module.exports = router;
